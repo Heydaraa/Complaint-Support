ef93e00e694102ee2ff67897be9ca49ae14ca048
HayiderHasan18 <hayiderhasan26@gmail.com>
2026-03-29 20:44:13 +0300

    changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL + "/api",
 });
-
+console.log("API URL:", import.meta.env.VITE_API_URL);
 api.interceptors.request.use((config) => {
   const token = sessionStorage.getItem("token");
   if (token) {
